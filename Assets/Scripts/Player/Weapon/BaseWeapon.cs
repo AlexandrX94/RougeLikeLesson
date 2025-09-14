c8b7f1aa67ee52f568a81ea0626ccdc61f703361
AlexandrX94 <alexkuzvas@gmail.com>
2025-09-14 14:03:24 +0300

    Module 9.3 problem

--- Assets/Scripts/Player/Weapon/BaseWeapon.cs
+++ Assets/Scripts/Player/Weapon/BaseWeapon.cs
@@ -1,8 +1,10 @@
 using Enemy;
+using GameCore;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Zenject;
+
 
 namespace Player.Weapon
 {
@@ -11,32 +13,31 @@ namespace Player.Weapon
         [SerializeField] private List<WeaponStats> _weaponStats = new List<WeaponStats>();
         [SerializeField] private float _damage;
         private DiContainer _diContainer;
-        private int _currentHealt = 1;
-        private int _maxHealt = 8;
+        private int _currentLevel = 1;
+        private int _maxLevel = 8;
         public List<WeaponStats> WeaponStats => _weaponStats;
+        public int CurrentLevel => _currentLevel;
+        public int MaxLevel => _maxLevel;
         public float Damage => _damage;
-        public int CurrentHealt => _currentHealt;
-        public int MaxHealt => _maxHealt;
 
         private void Awake()
         {
             _diContainer.Inject(this);
         }
 
-        private void Start()
+        protected virtual void Start()
         {
             SetStats(0);
         }
 
         public virtual void LevelUp()
         {
-            if(_currentHealt < _maxHealt)
-            {
-                _currentHealt++;
-            }
-            SetStats(_currentHealt - 1);
-        }
+            if (_currentLevel < _maxLevel)
+                _currentLevel++;
 
+                SetStats(_currentLevel - 1);
+            
+        }
         protected virtual void OnTriggerEnter2D(Collider2D other)
         {
             if(other.gameObject.TryGetComponent(out EnemyHealth enemy))
@@ -44,18 +45,15 @@ namespace Player.Weapon
                 float damage = Random.Range(_damage / 2f, _damage * 2f);
                 enemy.TakeDamage(damage);
             }
-
         }
 
-        protected virtual void SetStats(int value)
-        {
-            _damage = _weaponStats[value].damage;
-        }
+        protected virtual void SetStats(int value) => _damage = _weaponStats[value].Damage;
 
         [Inject] private void Construct(DiContainer diContainer)
         {
-            _diContainer = diContainer;
+            diContainer = _diContainer;
         }
+
     }
 }
 
---
+++ Assets/Scripts/Player/Weapon/FireballWeapon.cs
@@ -0,0 +1,118 @@
+using GameCore;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player.Weapon
+{
+    public class FireballWeapon : BaseWeapon, IActivate
+    {
+
+
+        [Header("Single")]
+        [SerializeField] private SpriteRenderer _spriteRenderer1X;
+        [SerializeField] private Collider2D _collider1X;
+        [SerializeField] private Transform _transformSprite1X, _targetContainer1X;
+
+        [Header("Double")]
+        [SerializeField] private List<SpriteRenderer> _spriteRenderer2X = new List<SpriteRenderer>();
+        [SerializeField] private List<Collider2D> _colleder2X = new List<Collider2D>();
+        [SerializeField] private List<Transform> _transformSprite2X = new List<Transform>();
+        [SerializeField] private Transform _targetContainer2X;
+
+        private WaitForSeconds _duration, _interval, _timeBetweenAtack;
+        private float _rotationSpeed, _range;
+        private Coroutine _attackCoroutine;
+
+        protected override void Start()
+        {
+            base.Start();
+            SetStats(0);
+            SetupWeapon();
+            Activate();
+        }
+
+        private void Update()
+        {
+            transform.Rotate(0, 0, _rotationSpeed * Time.deltaTime);
+        }
+
+        public override void LevelUp()
+        {
+            base.LevelUp();
+            SetupWeapon();
+        }
+
+        protected override void SetStats(int value)
+        {
+            base.SetStats(value);
+            _rotationSpeed = WeaponStats[CurrentLevel - 1].Speed;
+            _range = WeaponStats[CurrentLevel - 1].Range;
+            _duration = new WaitForSeconds(WeaponStats[CurrentLevel - 1].Duration);
+            _timeBetweenAtack = new WaitForSeconds(WeaponStats[CurrentLevel - 1].TimeBetweenAttack);
+        }
+
+        private void SetupWeapon()
+        {
+            if (CurrentLevel < 4)
+            {
+                _targetContainer1X.gameObject.SetActive(true);
+                _targetContainer2X.gameObject.SetActive(false);
+                _transformSprite1X.localPosition = new Vector3(_range, 0, 0);
+                _collider1X.offset = new Vector2(_range, 0);
+                
+            }
+            else
+            {
+                _targetContainer1X.gameObject.SetActive(false);
+                _targetContainer2X.gameObject.SetActive(true);
+                for (int i = 0; i < _colleder2X.Count; i++)
+                {
+                    _colleder2X[i].gameObject.SetActive(true);
+                }
+                _transformSprite2X[0].localPosition = new Vector2(_range, 0);
+                _transformSprite2X[1].localPosition = new Vector2(-_range, 0);
+                _colleder2X[0].offset = new Vector2(_range, 0);
+                _colleder2X[1].offset = new Vector2(-_range, 0);
+            }
+
+        }
+
+        private IEnumerator WeaponLifeCycle()
+        {
+            while (enabled)
+            {
+                if (CurrentLevel < 4)
+                {
+                    _spriteRenderer1X.enabled = !_spriteRenderer1X.enabled;
+                    _collider1X.enabled = !_collider1X.enabled;
+                }
+                else
+                {
+                    for (int i = 0; i < _spriteRenderer2X.Count; i++)
+                    {
+                        _spriteRenderer2X[i].enabled = !_spriteRenderer2X[i].enabled;
+                        _colleder2X[i].enabled = !_colleder2X[i].enabled;
+                    }
+                }
+
+                _interval = _spriteRenderer1X.enabled || _spriteRenderer2X[0].enabled ? _duration : _timeBetweenAtack;
+                yield return _interval;
+            }
+
+        }
+
+        public void Activate()
+        {
+            _attackCoroutine = StartCoroutine(WeaponLifeCycle());
+        }
+
+        public void Deactivate()
+        {
+            if (_attackCoroutine != null) StopCoroutine(_attackCoroutine);
+        }
+    }
+
+}
+
+
