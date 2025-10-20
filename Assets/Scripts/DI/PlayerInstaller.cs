05c38b7ad565c20726d6667eccd86bc3eb8dcccd
AlexandrX94 <alexkuzvas@gmail.com>
2025-10-20 23:48:00 +0300

    Module 10.1

--- Assets/Scripts/DI/PlayerInstaller.cs
+++ Assets/Scripts/DI/PlayerInstaller.cs
@@ -1,4 +1,5 @@
 using Player;
+using Player.Weapon.Suriken;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,10 +11,12 @@ namespace DI
     {
         [SerializeField] private PlayerMovement _playerMovement;
         [SerializeField] private PlayerHealth _playerHealth;
+        [SerializeField] private SurikenWeapon _surikenWeapon;
         public override void InstallBindings()
         {
             Container.Bind<PlayerMovement>().FromInstance(_playerMovement).AsSingle().NonLazy();
             Container.Bind<PlayerHealth>().FromInstance(_playerHealth).AsSingle().NonLazy();
+            Container.Bind<SurikenWeapon>().FromInstance(_surikenWeapon).AsSingle().NonLazy();
         }
     }
 }
---
+++ Assets/Scripts/Player/Weapon/Projectile.cs
@@ -0,0 +1,37 @@
+using Enemy;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player.Weapon
+{
+    public abstract class Projectile : MonoBehaviour
+    {
+        protected WaitForSeconds Timer;
+        protected float Damage;
+
+        protected virtual void OnEnable()
+        {
+            StartCoroutine(TimerToHide());
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if(other.gameObject.TryGetComponent(out EnemyHealth enemy))
+            {
+                float damage = Random.Range(Damage / 2, Damage * 2);
+                if (damage < 1)
+                { 
+                    damage = 1; 
+                }
+                enemy.TakeDamage(damage);
+            }
+        }
+        private IEnumerator TimerToHide()
+        {
+            yield return Timer;
+            gameObject.SetActive(false);
+        }
+    }
+}
+
